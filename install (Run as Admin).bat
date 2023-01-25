67913e2bc8604e55603624612324ea28ec7e3cd6
Ziad <62176660+InsertX2k@users.noreply.github.com>
2023-01-25 22:43:39 +0200

    Add files via upload

--- install (Run as Admin).bat
+++ install (Run as Admin).bat
@@ -1,13 +1,16 @@
 @echo off
 SET scriptpath=%~dp0
 cd /d %scriptpath%
-xcopy devrestart "%windir%\devrestart" /F /Y /H /I
-xcopy nssm-2.24\win64\nssm.exe "%windir%\devrestart\nssm.exe" /Y /H /R /I
-%windir%\devrestart\nssm.exe install "onboard_controller_fix" "%windir%\devrestart\devrestart.exe"
-reg add "HKEY_LOCAL_MACHINE\SYSTEM\CurrentControlSet\Services\onboard_controller_fix" /v "ErrorControl" /t REG_DWORD /f /d 0
-reg add "HKEY_LOCAL_MACHINE\SYSTEM\CurrentControlSet\Services\onboard_controller_fix" /v "Start" /t REG_DWORD /f /d 2
-reg add "HKEY_LOCAL_MACHINE\SYSTEM\CurrentControlSet\Services\onboard_controller_fix\Parameters\AppExit" /ve /f /d "Exit" /t REG_SZ
+xcopy devrestart2 "%windir%\devrestart2" /F /Y /H /I /E
+xcopy nssm-2.24\win64\nssm.exe "%windir%\devrestart2\nssm.exe" /Y /H /R /I
+%windir%\devrestart2\nssm.exe install "devrestart" "%windir%\devrestart2\devrestart2.exe"
+reg add "HKEY_LOCAL_MACHINE\SYSTEM\CurrentControlSet\Services\devrestart" /v "ErrorControl" /t REG_DWORD /f /d 0
+reg add "HKEY_LOCAL_MACHINE\SYSTEM\CurrentControlSet\Services\devrestart" /v "Start" /t REG_DWORD /f /d 2
+reg add "HKEY_LOCAL_MACHINE\SYSTEM\CurrentControlSet\Services\devrestart\Parameters\AppExit" /ve /f /d "Exit" /t REG_SZ
+reg add "HKLM\SYSTEM\CurrentControlSet\Services\devrestart" /v "DisplayName" /t REG_SZ /d "devrestart2 and its sub-services" /f
+sc config devrestart displayName= "devrestart2 Handler Windows NT Service"
 echo.
 echo All pending operations completed, press any key to close this window
+echo You may restart your computer for the service to start.
 pause >nul
 exit /b 0
